8722028d9c1dd7e919d8b4520268b81930184782
Serafima007 <123898263+Serafima007@users.noreply.github.com>
2023-02-27 10:58:30 +0200

    задача 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            int[] array = new int[15];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-50, 50);
+                Console.WriteLine($"array[{i}]={array[i]}");
+            }
+            byte count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Количество элементов массива,кратных 3,={count}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
