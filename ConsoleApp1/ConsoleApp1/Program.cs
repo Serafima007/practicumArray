a9a9a9c43b52ee7814f34fc3c6e620cba8980666
Serafima007 <123898263+Serafima007@users.noreply.github.com>
2023-03-05 20:02:19 +0200

    задание 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,22 +11,56 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Задача 1
-            int[] array = new int[15];
-            Random random = new Random();
+            //int[] array = new int[15];
+            //Random random = new Random();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = random.Next(-50, 50);
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            //byte count = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (array[i] % 3 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine($"Количество элементов массива,кратных 3,={count}");
+
+
+
+            //Задача 2
+            int max = 0;
+            int min = 0;
+            int[] array = new int[10];
+            Random rnd = new Random();
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = random.Next(-50, 50);
-                Console.WriteLine($"array[{i}]={array[i]}");
+                array[i] = rnd.Next(0, 100);
+                Console.WriteLine();
             }
-            byte count = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] % 3 == 0)
+                for (int i = 0; i < array.Length; i++)
                 {
-                    count++;
+                    if (array[min] > array[i])
+                    {
+                        min = i;
+                    }
+                    if (array[max] < array[i])
+                    {
+                        max = i;
+                    }
                 }
+            int a = array[min];
+            array[min] = array[max];
+            array[max] = a;
+            Console.WriteLine();
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
             }
-            Console.WriteLine($"Количество элементов массива,кратных 3,={count}");
+
+
 
             Console.ReadKey();
 
